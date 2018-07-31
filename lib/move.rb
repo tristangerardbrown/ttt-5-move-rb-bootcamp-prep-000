484cb02662ccc5be7a24d983a649522931cbf812
Tristan Brown <tgbrown@stanford.edu>
2018-07-31 18:03:48 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,6 +12,6 @@ def input_to_index(input)
   input = input.to_i - 1
 end
 
-def move(board, index, value="X")
+def move(board, input, value="X")
   board[input] = value
 end
