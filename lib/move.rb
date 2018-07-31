f8eeafe22f0fb8282e711b7c967d1665069ca9c2
Tristan Brown <tgbrown@stanford.edu>
2018-07-31 17:54:25 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def input_to_index(input)
   input = input.to_i - 1
 end
